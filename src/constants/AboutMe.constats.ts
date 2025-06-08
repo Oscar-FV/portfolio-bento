49508f8adfec67a74ca59ccef83c10c76f12cdb1
Oscar-FV <oscar.flores.vqz@gmail.com>
2025-06-08 15:13:17 -0600

    ft: added content and started to seaparate components by sections

---
+++ src/constants/AboutMe.constats.ts
@@ -0,0 +1,5 @@
+export const ABOUT_ME = [
+    "Hey! I’m Oscar — fullstack developer from San Luis Potosí, Mexico. I build immersive web experiences using Python, React, and Next.js ((Angular and I had a thing... but it’s complicated 😅).",
+    "I got into tech thanks to video games and stuck around because I love turning ideas into sleek, user-focused apps. Front-end is my jam — I enjoy being the bridge between people and product.",
+    "Off the keyboard, I’m into competitive Valorant, long city walks, and hunting down the best espresso tonic in town. Always open to new projects and good convos — let’s connect!"
+]
---
+++ src/constants/Skills.constants.ts
@@ -0,0 +1,13 @@
+import { Code2, Database, Globe, Palette, Smartphone } from "@lucide/astro";
+
+export const SKILLS  = [
+    { name: "React", icon: Code2, color: "text-blue-400" },
+    { name: "Next.js", icon: Globe, color: "text-gray-300" },
+    { name: "Angular", icon: Code2, color: "text-red-400" },
+    { name: "React Native", icon: Smartphone, color: "text-blue-300" },
+    { name: "TypeScript", icon: Code2, color: "text-blue-500" },
+    { name: "Python", icon: Code2, color: "text-yellow-400" },
+    { name: "Django", icon: Globe, color: "text-green-400" },
+    { name: "PostgreSQL", icon: Database, color: "text-blue-600" },
+    { name: "UX/UI", icon: Palette, color: "text-purple-400" },
+]
